be890c1e7b133fc46246d79b0c28c76cd280be9a
Juan Carlos Tinoco Licas <pcsijtin@upc.edu.pe>
2023-05-14 18:28:40 -0500

    Implementamos SaveCategoryResource y SaveTutorialResource que definen la data a recibir en caso de un consumo del API

---
+++ LearningCenter.API/Learning/Resources/SaveTutorialResource.cs
@@ -0,0 +1,10 @@
+namespace LearningCenter.API.Learning.Resources;
+
+public class SaveTutorialResource
+{
+    public string Title { get; set; }
+    
+    public string Description { get; set; }
+    
+    public int CategoryId { get; set; }
+}
