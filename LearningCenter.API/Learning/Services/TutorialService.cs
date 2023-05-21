0514d4ab5eaaaf1e4241a780b5b37e767a6f45ec
Juan Carlos Tinoco Licas <pcsijtin@upc.edu.pe>
2023-05-21 12:30:23 -0500

    Generamos la implementacion del ITutorialService en TutorialService

---
+++ LearningCenter.API/Learning/Services/TutorialService.cs
@@ -0,0 +1,45 @@
+﻿using LearningCenter.API.Learning.Domain.Models;
+using LearningCenter.API.Learning.Domain.Repositories;
+using LearningCenter.API.Learning.Domain.Services;
+using LearningCenter.API.Learning.Domain.Services.Communication;
+
+namespace LearningCenter.API.Learning.Services;
+
+public class TutorialService : ITutorialService
+{
+    private readonly ITutorialRepository _tutorialRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly ICategoryRepository _categoryRepository;
+
+    public TutorialService(ITutorialRepository tutorialRepository, IUnitOfWork unitOfWork, ICategoryRepository categoryRepository)
+    {
+        _tutorialRepository = tutorialRepository;
+        _unitOfWork = unitOfWork;
+        _categoryRepository = categoryRepository;
+    }
+
+    public Task<IEnumerable<Tutorial>> ListAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Tutorial>> ListByCategoryIdAsync(int categoryId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TutorialResponse> SaveAsync(Tutorial tutorial)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TutorialResponse> UpdateAsync(int tutorialId, Tutorial tutorial)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TutorialResponse> DeleteAsync(int tutorialId)
+    {
+        throw new NotImplementedException();
+    }
+}
