b0afbef6265c504d4c4084d5eccfd22518192971
Juan Carlos Tinoco Licas <pcsijtin@upc.edu.pe>
2023-05-28 18:46:30 -0500

    Generamos la implementación en UserRepository

---
+++ LearningCenter.API/Security/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,53 @@
+﻿using LearningCenter.API.Security.Domain.Models;
+using LearningCenter.API.Security.Domain.Repositories;
+using LearningCenter.API.Shared.Persistence.Contexts;
+using LearningCenter.API.Shared.Persistence.Repositories;
+
+namespace LearningCenter.API.Security.Persistence.Repositories;
+
+public class UserRepository :BaseRepository, IUserRepository
+{
+    public UserRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public Task<IEnumerable<User>> ListAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> FindByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> FindByUsernameAsync(string username)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool ExistsByUsername(string username)
+    {
+        throw new NotImplementedException();
+    }
+
+    public User FindById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(User user)
+    {
+        throw new NotImplementedException();
+    }
+}
