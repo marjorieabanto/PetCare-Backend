0e7e26bb1dfc69079ffbfa159873ab64cedee867
Juan Carlos Tinoco Licas <pcsijtin@upc.edu.pe>
2023-05-28 22:28:13 -0500

    Generamos los objetos AppSettings, IJwtHandler y JwtHandler

---
+++ LearningCenter.API/Security/Authorization/Handlers/Interfaces/IJwtHandler.cs
@@ -0,0 +1,9 @@
+﻿using LearningCenter.API.Security.Domain.Models;
+
+namespace LearningCenter.API.Security.Authorization.Handlers.Interfaces;
+
+public interface IJwtHandler
+{
+    public string GenerateToken(User user);
+    public int? ValidateToken(string token);
+}
