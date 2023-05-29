789d4ef16f2e8dac8bbe964cbd527c62d1909662
Juan Carlos Tinoco Licas <pcsijtin@upc.edu.pe>
2023-05-28 20:53:13 -0500

    Adicion de dependencias, clase que controlas las excepcionese y actualización del UserService

--- LearningCenter.API/Security/Services/UserService.cs
+++ LearningCenter.API/Security/Services/UserService.cs
@@ -4,6 +4,8 @@ using LearningCenter.API.Security.Domain.Models;
 using LearningCenter.API.Security.Domain.Repositories;
 using LearningCenter.API.Security.Domain.Services;
 using LearningCenter.API.Security.Domain.Services.Communication;
+using LearningCenter.API.Security.Exceptions;
+using BCryptNet = BCrypt.Net.BCrypt;
 
 namespace LearningCenter.API.Security.Services;
 
@@ -26,28 +28,89 @@ public class UserService : IUserService
         throw new NotImplementedException();
     }
 
-    public Task<IEnumerable<User>> ListAsync()
+    public async Task<IEnumerable<User>> ListAsync()
     {
-        throw new NotImplementedException();
+        return await _userRepository.ListAsync();
     }
 
-    public Task<User> GetByIdAsync(int id)
+    public async Task<User> GetByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        var user = await _userRepository.FindByIdAsync(id);
+        if (user == null) throw new KeyNotFoundException("User not found");
+        return user;
     }
 
-    public Task RegisterAsync(RegisterRequest model)
+    public async Task RegisterAsync(RegisterRequest request)
     {
-        throw new NotImplementedException();
+        // validate
+        if (_userRepository.ExistsByUsername(request.Username)) 
+            throw new AppException("Username '" + request.Username + "' is already taken");
+
+        // map model to new user object
+        var user = _mapper.Map<User>(request);
+
+        // hash password
+        user.PasswordHash = BCryptNet.HashPassword(request.Password);
+
+        // save user
+        try
+        {
+            await _userRepository.AddAsync(user);
+            await _unitOfWork.CompleteAsync();
+        }
+        catch (Exception e)
+        {
+            throw new AppException($"An error occurred while saving the user: {e.Message}");
+        }
     }
 
-    public Task UpdateAsync(int id, UpdateRequest model)
+    public async Task UpdateAsync(int id, UpdateRequest request)
     {
-        throw new NotImplementedException();
+        var user = GetById(id);
+
+        // Validate
+        if (_userRepository.ExistsByUsername(request.Username)) 
+            throw new AppException("Username '" + request.Username + "' is already taken");
+
+        // Hash password if it was entered
+        if (!string.IsNullOrEmpty(request.Password))
+            user.PasswordHash = BCryptNet.HashPassword(request.Password);
+
+        // Copy model to user and save
+        _mapper.Map(request, user);
+        try
+        {
+            _userRepository.Update(user);
+            await _unitOfWork.CompleteAsync();
+        }
+        catch (Exception e)
+        {
+            throw new AppException($"An error occurred while updating the user: {e.Message}");
+        }
     }
 
-    public Task DeleteAsync(int id)
+    public async Task DeleteAsync(int id)
     {
-        throw new NotImplementedException();
+        var user = GetById(id);
+            
+        try
+        {
+            _userRepository.Remove(user);
+            await _unitOfWork.CompleteAsync();
+        }
+        catch (Exception e)
+        {
+            throw new AppException($"An error occurred while deleting the user: {e.Message}");
+        }
     }
+    // helper methods
+
+    private User GetById(int id)
+    {
+        var user = _userRepository.FindById(id);
+        if (user == null) throw new KeyNotFoundException("User not found");
+        return user;
+    }
+    
+    
 }
